7e556c4629756b9fd0505d3f0e3d14da16daa3cd
vladimir.netkachev <vladimir.netkachev@akvelon.com>
2017-02-06 18:55:05 +0300

    Initial commit

--- ProbaIosBuild/ViewController.swift
+++ ProbaIosBuild/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func buttonTouch(_ sender: Any) {
+        if let lbl = label, let lblText = label.text {
+            lbl.text = lblText + "la"
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +27,4 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
--- ProbaIosBuildUITests/ProbaIosBuildUITests.swift
+++ ProbaIosBuildUITests/ProbaIosBuildUITests.swift
@@ -29,8 +29,10 @@ class ProbaIosBuildUITests: XCTestCase {
     }
     
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCUIApplication().buttons.element(boundBy: 0).tap()
+        XCUIApplication().buttons.element(boundBy: 0).tap()
+        XCUIApplication().buttons.element(boundBy: 0).tap()
+        XCUIApplication().buttons.element(boundBy: 0).tap()
     }
     
 }
